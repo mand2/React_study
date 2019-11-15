047ed21c75dce752a6b9df21d762976a1f848750
Nayeon Kim <nayeon.kim1123@gmail.com>
2019-11-15 11:53:26 +0900

    mobx로 리액트만들기 3: inject로 바꾸기
    src/index.js 에서 const로 'counter' 라 명명했기 때문에
    inject 시 counter 라고 써줘야 한다.
    CounterStore / counterStore 등 바로 store에서 주입  ㄴㄴ임 아직은.

--- counter/mobx-with-react/src/Counter.js
+++ counter/mobx-with-react/src/Counter.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
-import { observable, action } from 'mobx';
-import { observer } from 'mobx-react';
+import { observer, inject } from 'mobx-react';
 
+@inject('counter') //index.js 에서 counter로 설정했기 때문에 counter로 불러온다
 @observer
 class Counter extends Component {
-  @observable
-  number = 0;
-
-  @action
-  increase = () => {
-    this.number++;
-  }
-  @action
-  decrease = () => {
-    this.number--;
-  }
-
   render() {
+    const { counter } = this.props;
     return (
       <div>
-        <h1>{this.number}</h1>
-        <button onClick={this.increase}>+1</button>
-        <button onClick={this.decrease}>-1</button>
+        <h1>{counter.number}</h1>
+        <button onClick={counter.increase}>+1</button>
+        <button onClick={counter.decrease}>-1</button>
       </div>
     );
   }
