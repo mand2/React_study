8046ee605aaac596dbdd32438e08d65c5ac0ff47
Nayeon Kim <nayeon.kim1123@gmail.com>
2019-11-15 10:35:07 +0900

    mobx로 리액트만들기 2: provider 써보기

--- counter/mobx-with-react/src/index.js
+++ counter/mobx-with-react/src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'mobx-react'; // MobX 에서 사용하는 Provider
+import CounterStore from './stores/CounterStore'; // 방금 만든 스토어 불러와줍니다.
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const counter = new CounterStore(); // 스토어 인스턴스를 만들고
+
+ReactDOM.render(
+  <Provider counter={counter}>
+    {/* Provider 에 props 로 넣어줍니다. */}
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+// 이전
+// ReactDOM.render(<App />, document.getElementById('root'));
+// serviceWorker.unregister();
